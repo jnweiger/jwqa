0f5b71b2726a1709e76cf551c7548a582597f23b
Jürgen Weigert <juergenweigert68@gmail.com>
2025-12-01 22:42:15 +0100

    first working version of hetzner_dns.sh We can add, delete, list. The del command needs some improvement, it should be able to delete all records for a given ip address, or all records for a given name, when only one of them is given.

---
+++ tools/hetzner_dns.sh
@@ -0,0 +1,95 @@
+#! /bin/bash
+#
+# requires:
+#	export HCLOUD_TOKEN=...
+#	export HCLOUD_DNS_ZONE=jwqa.de	# or defaults to finding the zone by suffix match of the fqdn name.
+
+verbose=true	# true,false
+
+if [ -n "$1" -a "$1" != "add" -a "$1" != "del" -a "$1" != "list" ]; then
+  cat <<EOF 1>&2
+Usage: 
+
+	export HCLOUD_TOKEN=...
+	export HCLOUD_DNS_ZONE=...
+	$0 [list]		# list all records in the zone
+	$0 add NAME IP_ADDR 	# add a new A record. Name can be FQDN or a plain domain name, if HCLOUD_DNS_ZONE is set.
+	$0 del NAME|IP_ADDR	# records can be deleted by IP_ADDR or by NAME.
+EOF
+  exit 0
+fi
+
+if [ -z "$HCLOUD_TOKEN" ]; then
+  echo 1>&2 "$0 ERROR: env variable HCLOUD_TOKEN is not set."
+  echo 1>&2 "\t Visit https://console.hetzner.com -> Select your Project -> Security -> API-Tokens -> Add ..."
+  exit 1
+fi
+vers=$(hcloud version 2>/dev/null)
+if [ -z "$vers" ]; then
+  echo 1>&2 "$0 ERROR: hcloud comand is not installed correctly."
+  echo 1>&2 "\t Visit https://github.com/hetznercloud/cli/releases/latest"
+  echo 1>&2 "\t or try:	go install github.com/hetznercloud/cli/cmd/hcloud@latest"
+  exit 1
+fi
+
+if [ -z "$1" -o "$1" == "list" ]; then
+  if [ -z "$HCLOUD_DNS_ZONE" ]; then
+    echo 1>&2 "$0 ERROR: env variable HCLOUD_DNS_ZONE is not set, cannot list records without a zone."
+    exit 1
+  fi
+  hcloud zone rrset list $HCLOUD_DNS_ZONE
+  if [ -z "$1" ]; then
+    echo 1>&2 ""
+    echo 1>&2 "use:		$0 add|del [IP_ADDR] [FQDN]"
+  fi  
+fi  
+
+# sets global variable name and possibly also HCLOUD_DNS_ZONE
+set_name_and_check_zone()
+{
+  name="$1"
+  test -z "$name" && return
+
+  if [ -z "$HCLOUD_DNS_ZONE" ]; then
+    # we have no explizit zone name, try to match one.
+    zones="$(hcloud zone list -o columns=NAME -o noheader)"
+    if [ -z "$zones" ]; then
+      echo 1>&2 "$0 ERROR: nothing found: hcloud zone list"
+      exit 1
+    fi
+    $verbose && echo 1>&2 "Zones managed here:" $zones
+    for z in $zones; do
+      if [[ $name = *.$z ]]; then
+        HCLOUD_DNS_ZONE="$z"
+      fi
+    done
+    if [ -z "$HCLOUD_DNS_ZONE" ]; then
+      echo 1>&2 "$0 ERROR: $name is not an FQDN in:" $zones
+      echo 1>&2 "Please give a full qualified domain name, or set HCLOUD_DNS_ZONE."
+      exit 1
+    fi 
+  fi
+  name="${name%.$HCLOUD_DNS_ZONE}"	# strip the zone name, if included.
+}
+
+if [ "$1" == "add" ]; then
+  set_name_and_check_zone "$2"
+  ipaddr="$3"
+  if [ -z "$name" -o -z "$ipaddr" ]; then
+    echo 1>&2 "$0 ERROR: add needs two parameters: NAME IP_ADDR"
+    exit 1
+  fi
+  (set -x; hcloud zone rrset add-records --record $ipaddr $HCLOUD_DNS_ZONE $name A)
+  exit 0
+fi
+
+if [ "$1" == "del" ]; then
+  set_name_and_check_zone "$2"
+  ipaddr="$3"
+  if [ -z "$name" -o -z "$ipaddr" ]; then
+    echo 1>&2 "$0 ERROR: sorry, del currently needs two parameters: NAME IP_ADDR"
+    exit 1
+  fi
+  (set -x; hcloud zone rrset remove-records --record $ipaddr $HCLOUD_DNS_ZONE $name A)
+  exit 0
+fi
