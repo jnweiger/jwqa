663dd6c4dc3a8e2e2531fab2f10cb3c2b8299bb7
Juergen Weigert <j.weigert@heinlein-support.de>
2025-12-02 18:06:01 +0100

    hetzner_deploy.sh can do certbot. but DNS Names are still TODO.

---
+++ tools/oc/clamav.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#
+# DNS_NAME:     av-DATE
+# DNS_NAME:     clamav-DATE
+# DNS_NAME:     icap-DATE
+# HCLOUD_IMAGE: ubuntu-22.04
+# HCLOUD_TYPE:  cpx21		# cpx11 2GB, cpx21 4GB, cpx41 8G
+#
+
+uname -a
+
+export CLAMAV_DUMMY=started_with_nothing
+
