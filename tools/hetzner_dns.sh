0ec3b92aed07f793bfc4c05d060b0847a02e5aba
Jürgen Weigert <juergenweigert68@gmail.com>
2026-01-08 14:24:28 +0100

    improved usability and add docs to hetzner_dns.sh Also add a schema extension for openldap, so that we can identify checkmk admins, users and guests by attributes.

--- tools/hetzner_dns.sh
+++ tools/hetzner_dns.sh
@@ -3,16 +3,26 @@
 # requires:
 #	export HCLOUD_TOKEN=...
 #	export HCLOUD_DNS_ZONE=jwqa.de	# or defaults to finding the zone by suffix match of the fqdn name.
+#
+# hetzner_dns.sh is a wrapper script around the hcloud command, to
+# a) improve userfriedliness. E.g by allowing both names with and without a domain suffix.
+# b) hide complexity.
+#    - Some subcommands of hcloud, like e.g. 'hcloud zone rrset' are oddly named.
+#    - We focus on the three basic things, that I want to do with dns names: list, add, delete "A" records.
+#    - The add command definitly needs both a Name and an IP Address, to create a mapping.
+#    - The del command can remove multipe mappings if only an IP Address or a Name is specified.
+#      E.g. when destroying a host, this feature allows us to remove all Names that map to the hosts IP Address.
+# c) be transparent and educate users: add and del always print the corresponding hcloud commands they run.
 
 verbose=true	# true,false
 
 if [ -n "$1" -a "$1" != "add" -a "$1" != "del" -a "$1" != "list" ]; then
   cat <<EOF 1>&2
-Usage: 
+Usage:
 
 	export HCLOUD_TOKEN=...
 	export HCLOUD_DNS_ZONE=...
-	$0 [list]		# list all records in the zone
+	$0 [list [zone]]	# list all records in the zone
 	$0 add NAME IP_ADDR 	# add a new A record. Name can be FQDN or a plain domain name, if HCLOUD_DNS_ZONE is set.
 	$0 del NAME|IP_ADDR	# records can be deleted by IP_ADDR or by NAME.
 EOF
@@ -28,21 +38,25 @@ vers=$(hcloud version 2>/dev/null)
 if [ -z "$vers" ]; then
   echo 1>&2 "$0 ERROR: hcloud comand is not installed correctly."
   echo 1>&2 "	Visit https://github.com/hetznercloud/cli/releases/latest"
-  echo 1>&2 "	or try:	go install github.com/hetznercloud/cli/cmd/hcloud@latest"
+  echo 1>&2 "	or try:	 go install github.com/hetznercloud/cli/cmd/hcloud@latest"
   exit 1
 fi
 
-if [ -z "$1" -o "$1" == "list" ]; then
+if [ -z "$1" -o "$1" == "list" -o "$1" == "ls" ]; then
+  if [ -n "$1" -a -n "$2" ]; then
+    HCLOUD_DNS_ZONE="$2"
+  fi
   if [ -z "$HCLOUD_DNS_ZONE" ]; then
     echo 1>&2 "$0 ERROR: env variable HCLOUD_DNS_ZONE is not set, cannot list records without a zone."
+    echo 1>&2 "   To see the zones you can manage here, try:  hcloud zone list"
     exit 1
   fi
-  hcloud zone rrset list $HCLOUD_DNS_ZONE
+  hcloud zone rrset list -- $HCLOUD_DNS_ZONE || echo 1>&2 "   To see the zones you can manage here, try:  hcloud zone list"
   if [ -z "$1" ]; then
     echo 1>&2 ""
     echo 1>&2 "use:		$0 add|del [FQDN] [IP_ADDR]"
-  fi  
-fi  
+  fi
+fi
 
 # sets global variable name and possibly also HCLOUD_DNS_ZONE
 set_name_and_check_zone()
@@ -58,16 +72,16 @@ set_name_and_check_zone()
       exit 1
     fi
     $verbose && echo 1>&2 "Zones managed here:" $zones
-    for z in $zones; do
-      if [[ $name = *.$z ]]; then
-        HCLOUD_DNS_ZONE="$z"
+    for zone in $zones; do
+      if [[ $name = *.$zone ]]; then
+        HCLOUD_DNS_ZONE="$zone"
       fi
     done
     if [ -z "$HCLOUD_DNS_ZONE" ]; then
       echo 1>&2 "$0 ERROR: $name is not an FQDN in:" $zones
       echo 1>&2 "Please give a full qualified domain name, or set HCLOUD_DNS_ZONE."
       exit 1
-    fi 
+    fi
   fi
   name="${name%.$HCLOUD_DNS_ZONE}"	# strip the zone name, if included.
 }
@@ -100,7 +114,7 @@ if [ "$1" == "del" ]; then
 
   a_records=$(hcloud zone rrset list jwqa.de -o json | jq -r '.[] | select(.type == "A") | "\"\(.name)\"\t\([.records[].value | tostring])"')
   # $verbose && echo "$a_records"
-  
+
   matches=$(echo "$a_records" | grep "\"$name\"" | tr -d '"[]')
   # av      46.62.160.228
   # foo     46.62.160.228
