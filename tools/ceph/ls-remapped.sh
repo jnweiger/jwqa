12744258c3bf31dbf41e06723056094a63a248d1
Juergen Weigert <j.weigert@heinlein-support.de>
2026-05-06 20:20:14 +0200

    beautify json via sed ..

--- tools/ceph/ls-remapped.sh
+++ tools/ceph/ls-remapped.sh
@@ -2,10 +2,13 @@
 #
 
 osd="$1"
+# set -x
 
-set -x
 if [ -z "$osd" ]; then
-  ceph pg ls remapped -f json | jq -c '.pg_stats[]? | { pgid, acting, up, state }'
+  ceph pg ls remapped -f json 
 else
-  ceph pg ls-by-osd "$osd" remapped -f json | jq -c '.pg_stats[]? | { pgid, acting, up, state }'
-fi
+  ceph pg ls-by-osd "$osd" remapped -f json 
+fi |\
+ jq -c '.pg_stats[]? | { pgid, acting, up, state }' | sed -e 's/,"/	"/g' | tr -d '{}"'
+
+
