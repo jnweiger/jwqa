bc66377f0ccdfd045056c91024e6e9a7e6446fcf
Juergen Weigert <j.weigert@heinlein-support.de>
2026-07-08 18:59:42 +0200

    vw_access_check.py now lists groups of users.

--- tools/vw_access_check.py
+++ tools/vw_access_check.py
@@ -9,12 +9,12 @@
 # 2026 (C) j.weigert@heinlein-support.de
 #
 
+import sys, os, json, subprocess, shlex
+
 server_ssh     = os.environ.get("VW_SERVER_SSH")    # username@host
 sqlite_db_path = os.environ.get("VW_DB_PATH", "vaultwarden/data/db.sqlite3")
 sqlite_cmd     = [ "ssh", server_ssh, "sqlite3", "-json", sqlite_db_path ]
 bw_cli         = os.environ.get("VW_BW_TOOL", "bw")
-
-import sys, os, json, subprocess, shlex
 
 def vw_sql(query):
     if not server_ssh:
@@ -41,9 +41,63 @@ def vw_cli(cmd):
 
 
 collection_list = vw_cli(["list", "collections"])
+# [{'object': 'collection', 'id': '389811e6-38db-460b-8eff-bd4a7fc0c8a7', 'organizationId': '9051a55c-e6d0-45bf-843e-7add02ef88b0', 'name': 'Default collection', 'externalId': None},
+#  {'object': 'collection', 'id': '8cec38c7-264b-49ff-af97-a8bdef50e146', 'organizationId': '9051a55c-e6d0-45bf-843e-7add02ef88b0', 'name': 'XXXXXXXXXXXXXXXXXXXXXXXX', 'externalId': None},
+#  {'object': 'collection', 'id': '1d408c16-43a6-4b13-97f3-74b0bd250b9f', 'organizationId': '9051a55c-e6d0-45bf-843e-7add02ef88b0', 'name': 'YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY', 'externalId': None},
+# ...
 
 user_list = vw_sql("select uuid,email,name from users")
+# [{'uuid': 'c4b9047d-f41c-499f-b26b-1caf83ed16cb', 'email': 'j.XXXXXXXXXXXXXXXXXXXXXXXXXXX', 'name': 'j.XXXXXXXXXXXXXXXXXXXXXXXXXXX'},
+#  {'uuid': '20d0f8be-2676-4d0b-bb0e-f051cc11072a', 'email': 't.XXXXXXXXXXXXXXXXXXXXXXXXXX', 'name': 't.XXXXXXXXXXXXXXXXXXXXXXXXXX'},
+#  {'uuid': '11db9933-aa89-41a4-ae31-c6e3cd1db8c1', 'email': 'testy@XXXXXXXXXXXXXXXXXXX', 'name': 'testy@XXXXXXXXXXXXXXXXXXX'}]
+# ...
+orguser_list       = vw_sql("select uuid,user_uuid from users_organizations")
+group_list         = vw_sql("select uuid,organizations_uuid,name,access_all from groups")
+group_orguser_list = vw_sql("select groups_uuid, users_organizations_uuid from groups_users")
 
-print(user_list)
-print(collection_list)
+user_email2uuid = { item['email']: item['uuid'] for item in user_list }
+user_uuid2email = { item['uuid']: item['email'] for item in user_list }
+user_uuid2name  = { item['uuid']: item['name'] for item in user_list }
+group_byuuid    = { item['uuid']: item         for item in group_list }
+
+user_uuid2orguuid  = { item['user_uuid']: item['uuid'] for item in orguser_list }
+
+collection_uuid2name = { item['id']: item['name'] for item in collection_list }
+collection_name2uuid = { item['name']: item['id'] for item in collection_list }
+
+user_orguuid2groups = {}
+for item in group_orguser_list:
+  u = item["users_organizations_uuid"]
+  if u not in user_orguuid2groups:
+    user_orguuid2groups[u] = [ item["groups_uuid"] ]
+  else:
+    user_orguuid2groups[u].append(item["groups_uuid"])
+
+
+print(user_email2uuid)
+print(user_orguuid2groups)
+# print(collection_name2uuid.keys())
+
+for user_email in user_email2uuid.keys():
+  user_uuid = user_email2uuid[user_email]
+  user_name = user_uuid2name[user_uuid]
+  if user_name == user_email:
+    user_print = user_email
+  else:
+    user_print = f"{user_name} <{user_email}>"
+
+  o = user_uuid2orguuid.get(user_uuid)
+  if not o:
+    print(f"user {user_print} not found in sqlite table users_organizations")
+    next
+
+  gl = user_orguuid2groups.get(o)
+  if not gl:
+    print(f"user {user_print} orguuid {o} not found in sqlite table groups_users")
+    next
+
+  print(f"groups of {user_print}:")
+  for g in gl:
+    print(f"\t{g} {group_byuuid[g]['name']}")
+
 
