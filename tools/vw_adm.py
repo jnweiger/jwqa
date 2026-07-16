84b802a5ebc202f05f8664225ced3802d1fc5af8
Juergen Weigert <j.weigert@heinlein-support.de>
2026-07-16 18:03:42 +0200

    vw_adm.py user_ops() completed. user invite is special, it also needs the Bearer Token of an admin web login. Auth of the public /admin api is strange.

--- tools/vw_adm.py
+++ tools/vw_adm.py
@@ -16,7 +16,7 @@
 #   without any preparation. It is unclear, if this is really safe while the server runs. It seems so.
 
 import sys, os, json, subprocess, shlex
-import argparse, uuid
+import argparse, uuid, requests
 
 
 server_ssh     = os.environ.get("VW_SERVER_SSH")    # username@host
@@ -26,7 +26,6 @@ bw_cli         = os.environ.get("VW_BW_TOOL", "bw")
 
 verbose = 1     # 0, 1, 2 babble more ore less while working.
 
-#!/usr/bin/env python3
 import argparse
 
 
@@ -48,7 +47,7 @@ def parse_args(argv):
     group_parser.add_argument("names", metavar="NAMES", nargs="*", help="one or more names (or emails or uuids).")
 
     user_parser = subparsers.add_parser("user", aliases=["u"], help="User operations. Try: user --help for details")
-    user_parser.add_argument("cmd", metavar="add|del|list", choices=("add", "del", "list"))
+    user_parser.add_argument("cmd", metavar="add|list|invite|del", choices=("add", "del", "list", "invite", "confirm"))
     user_parser.add_argument("email", metavar="EMAIL", nargs="?", help="E-Mail address or uuid to list or manipulate")
     user_parser.add_argument("names", metavar="NAMES", nargs="*", help="optional: Firstname Lastname ...")
 
@@ -162,7 +161,7 @@ def cmd_list_group_collections(args, guuid):
       collection_uuid2name = { item['id']: item['name'] for item in collection_name_list }
       if args.json:
         for c in collection_uuid_list:
-          c['name'] = collection_uuid2name[c['collections_uuid']] 
+          c['name'] = collection_uuid2name[c['collections_uuid']]
         print(json.dumps(collection_uuid_list))
       else:
         for c in collection_uuid_list:
@@ -210,6 +209,7 @@ def user_lookup(user_list, name):
 
 
 def map_user_uuid2orguuid(uu_list):
+  # CAUTION: This probably messes up when we have multiple organizations
   orguser_list = vw_sql("SELECT uuid, user_uuid FROM users_organizations")
   user_uuid2orguuid  = { item['user_uuid']: item['uuid'] for item in orguser_list }
   return [user_uuid2orguuid[uu] for uu in uu_list]
@@ -243,12 +243,12 @@ def assert_group(name):
     print(f"ERROR: cannot use --create with a group uuid. Group name needed.", file=sys.stderr)
     sys.exit(1)
   # INSERT OR IGNORE is not sufficient. Duplicate names are apparently permitted.grrr. Must check explicitly.
-  grp = vw_sql(f"SELECT uuid FROM groups WHERE name == '{name}'") 
-  if len(grp): 
+  grp = vw_sql(f"SELECT uuid FROM groups WHERE name == '{name}'")
+  if len(grp):
     if verbose: print(f"OK: group {name} already exists")
     return
 
-  org = vw_sql("SELECT uuid FROM organizations") 
+  org = vw_sql("SELECT uuid FROM organizations")
   guuid = str(uuid.uuid4())
   cmd = f"INSERT INTO groups (uuid, organizations_uuid, name, access_all, creation_date, revision_date) VALUES ('{guuid}', '{org[0]['uuid']}', '{name}', 0, datetime('now'), datetime('now'))";
   if verbose > 1: print(f"SQL: {cmd};")
@@ -265,12 +265,116 @@ def cmd_user_add(email, username):
   if len(u) > 0:
     print(f"OK: user <{email}> already exists. uuid={u[0]['uuid']}")
     return
-  print("not impl.", email, username)
+  org = vw_sql("SELECT uuid, name FROM organizations")
+  cli_status = vw_cli(["status"])
+  # {"serverUrl":"https://vw...","lastSync":"2026-07-06T20:43:58.991Z","userEmail":"j.weigert@...","userId":"c4b9047d-...","status":"unlocked"}
+
+  user_uuid = str(uuid.uuid4())
+  member_uuid = str(uuid.uuid4())
+  stamp = str(uuid.uuid4())
+  cols = "uuid, created_at, updated_at, email, name, password_hash, salt, password_iterations, akey, security_stamp, equivalent_domains, excluded_globals"
+  vals = f"'{user_uuid}',datetime('now'), datetime('now'),'{email}','{username}','',X'{os.urandom(64).hex()}',600000,'','{stamp}','[]','[]'"
+  cmd = f"INSERT INTO users ({cols}) VALUES ({vals})"
+  if verbose > 1: print(f"SQL: {cmd};", file=sys.stderr)
+  r = vw_sql(cmd)
+  if len(r): print(f"ERROR: {r}", file=sys.stderr)
+
+  status = 0    # -1=Revoked, 0=Invited, 1=Accepted, 2=Confirmed
+  atype = 2     # 0=Owner, 1=Admin, 2=User, 3=Manager, 4=Custom
+  inviter = "vw_adm+" + cli_status.get("userEmail", '')
+  cols = "uuid, user_uuid, org_uuid, access_all, akey, status, atype, invited_by_email"
+  vals = f"'{member_uuid}', '{user_uuid}', '{org[0]['uuid']}', 0, '', {status}, {atype}, '{inviter}'"
+  cmd = f"INSERT INTO users_organizations ({cols}) VALUES ({vals})"
+  if verbose > 1: print(f"SQL: {cmd};", file=sys.stderr)
+  r = vw_sql(cmd)
+  if len(r): print(f"ERROR: {r}", file=sys.stderr)
+
+  return
+
+
+def api_org_user_invite(base_url, org_uuid, user_uuid):
+  # curl 'https://vw-test.heinlein-support.de/api/organizations/9051a55c-e6d0-45bf-843e-7add02ef88b0/users/d2b52a02-9d41-4578-ad5b-1d4ed8364c5d/reinvite'   -X POST   -H "authorization: Bearer $VW_BEARER_TOKEN"
+  token = os.environ.get("VW_BEARER_TOKEN")    # from
+  if not token:
+    print("VW_BEARER_TOKEN environment variable is not set. Open Network Tab in browser console, right click a request and try 'Copy as Curl' ", file=sys.stderr)
+    return
+  url = f"{base_url}/api/organizations/{org_uuid}/users/{user_uuid}/reinvite"
+  headers = { "Authorization": f"Bearer {token}" }
+
+  if verbose > 0: print(f"POST {url};", file=sys.stderr)
+  response = requests.post(url, headers=headers)
+  response.raise_for_status()
+
+  # Return JSON if present, otherwise raw text
+  if response.headers.get("Content-Type", "").startswith("application/json"):
+    return response.json()
+  return response.text
+
+
+def api_org_user_confirm(base_url, org_uuid, user_uuid):
+  # curl 'https://vw-test.heinlein-support.de/api/organizations/9051a55c-e6d0-45bf-843e-7add02ef88b0/users/d2b52a02-9d41-4578-ad5b-1d4ed8364c5d/confirm' -X POST -H 'authorization: Bearer $VW_BEARER_TOKEN" --data-raw '{"key":"4.es6/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX==","defaultUserCollectionName":"2.02fJu7cBFUzX/trIsdiIWg==|sCZ2r2BMCAVa9Czo1ik9iw==|bzqy/fPiR8nEv7NMpc8dS/+QGORSLC71zHKiNrC6BXM="}'
+  #
+  # This should probably remain a manual step, for mutual identity verification.
+  print("not impl.")
+
+
+# def api_invite_resend(base_url, admin_token, id):
+#   # DRAFT... need the admin token from
+#   # /admin/ -> General Settings -> Admin token/Argon2 PHC -> Show (or something else?
+#   post_url = f"{base_url}/admin/users/{id}/invite/resend"
+#   headers = {
+#     "Content-Type": "application/json",
+#   }
+#   cookies = {
+#     "VW_ADMIN": admin_token,  # admin session cookie
+#   }
+#   response = requests.post(post_url, headers=headers, cookies=cookies)
+#   response.raise_for_status()
+#   return response
+
+
+def cmd_user_invite(email):
+  uu, err = user_lookup_all([email])
+  if err:
+    print(f"ERROR: {err} - try: user add ... ")
+    sys.exit(1)
+  ouu = map_user_uuid2orguuid(uu)
+  org = vw_sql("SELECT uuid FROM organizations")
+  cli_status = vw_cli(["status"])
+  api_org_user_invite(cli_status['serverUrl'], org[0]['uuid'], ouu[0])
+
+
+def cmd_user_delete(email):
+  uu, err = user_lookup_all([email])
+  if err:
+    print(f"ERROR: {err} - try: user add ... ")
+    sys.exit(1)
+  ouu = map_user_uuid2orguuid(uu)
+  cmd = f"DELETE FROM groups_users WHERE users_organizations_uuid == '{ouu[0]}'"
+  if verbose > 1: print(f"SQL: {cmd};", file=sys.stderr)
+  r = vw_sql(cmd)
+  if len(r): print(f"ERROR: {r}", file=sys.stderr)
+
+  cmd = f"DELETE FROM users_collections WHERE user_uuid == '{ouu[0]}'"
+  if verbose > 1: print(f"SQL: {cmd};", file=sys.stderr)
+  r = vw_sql(cmd)
+  if len(r): print(f"ERROR: {r}", file=sys.stderr)
+
+  cmd = f"DELETE FROM users_organizations WHERE uuid == '{ouu[0]}'"
+  if verbose > 1: print(f"SQL: {cmd};", file=sys.stderr)
+  r = vw_sql(cmd)
+  if len(r): print(f"ERROR: {r}", file=sys.stderr)
+
+  cmd = f"DELETE FROM users WHERE uuid == '{uu[0]}'"
+  if verbose > 1: print(f"SQL: {cmd};", file=sys.stderr)
+  r = vw_sql(cmd)
+  if len(r): print(f"ERROR: {r}", file=sys.stderr)
+
   return
 
 
 def user_ops(user_parser, args):
-  if verbose > 0: print("user_ops:", args, file=sys.stderr)
+  if verbose > 1: print("user_ops:", args, file=sys.stderr)
   # ./vw_adm.py user add juergen@fabmail.org Jürgen Weigert
   # -> (cmd='add', create=False, email='juergen@fabmail.org', json=False, list=False, names=['Jürgen', 'Weigert'], object='u', permission=None)
   if args.cmd == "add":
@@ -278,8 +382,37 @@ def user_ops(user_parser, args):
       print(f"ERROR: user add needs an email address. not {args.email}", file=sys.stderr)
       user_parser.print_help()
       user_parser.exit(3)
+    return cmd_user_add(args.email, " ".join(args.names))
 
-    cmd_user_add(args.email, " ".join(args.names))
+  if args.cmd == "invite":
+    if args.email is None:
+      print(f"ERROR: specify email address of an added user", file=sys.stderr)
+      sys.exit(1)
+    return cmd_user_invite(args.email)
+
+  if args.cmd == "del":
+    if args.email is None:
+      print(f"ERROR: specify email address of an added user", file=sys.stderr)
+      sys.exit(1)
+    return cmd_user_delete(args.email)
+
+  if args.cmd == "list":
+    user_list = vw_sql(f"SELECT uuid, name, email FROM users")
+    orguser_list = vw_sql("SELECT uuid, user_uuid FROM users_organizations")
+    user_uuid2orguuid  = { item['user_uuid']: item['uuid'] for item in orguser_list }
+    for u in user_list:
+      if u["uuid"] in user_uuid2orguuid:
+        u["member_uuid"] = user_uuid2orguuid[u["uuid"]]
+
+    if args.json:
+      print(json.dumps(user_list))
+    else:
+      for u in user_list:
+        print(f"\t{u['uuid']} {u['name']} <{u['email']}>")
+    return
+
+  print(f"ERROR: user_ops(cmd={args.cmd}) not impl.", file=sys.stderr)
+  return
 
 
 def main(argv=None):
@@ -317,7 +450,7 @@ def main(argv=None):
     if err:
       print(f"ERROR: {err}")
       sys.exit(1)
-      
+
     ouu = map_user_uuid2orguuid(uu)
     if verbose > 1: print([uu, ouu, err], file=sys.stderr)
 
@@ -375,15 +508,15 @@ def main(argv=None):
 
 #   group_orguser_list    = vw_sql("select groups_uuid, users_organizations_uuid from groups_users")
 #   collection_group_list = vw_sql("select collections_uuid, groups_uuid, read_only, hide_passwords from collections_groups")
-#   
+#
 #   user_email2uuid    = { item['email']: item['uuid'] for item in user_list }
 #   user_uuid2email    = { item['uuid']: item['email'] for item in user_list }
 #   user_uuid2name     = { item['uuid']: item['name'] for item in user_list }
-#   
-#   
+#
+#
 #   collection_uuid2name = { item['id']: item['name'] for item in collection_list }
 #   collection_name2uuid = { item['name']: item['id'] for item in collection_list }
-#   
+#
 #   user_orguuid2groups = {}
 #   for item in group_orguser_list:
 #     u = item["users_organizations_uuid"]
@@ -391,7 +524,7 @@ def main(argv=None):
 #       user_orguuid2groups[u] = [ item["groups_uuid"] ]
 #     else:
 #       user_orguuid2groups[u].append(item["groups_uuid"])
-#   
+#
 #   group_uuid2colls = {}
 #   for item in collection_group_list:
 #     g = item['groups_uuid']
